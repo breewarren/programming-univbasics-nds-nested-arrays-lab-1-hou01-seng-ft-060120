e70cf6d634a749ee49f4c3c3057532ed8af9e095
Bree Warren <ambreea.warren@gmail.com>
2020-05-16 20:42:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,8 +31,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix[0][1]
-  matrix_lookup
+  
 end
 
  # Given any matrix (array of arrays), a row index and a column index, 
