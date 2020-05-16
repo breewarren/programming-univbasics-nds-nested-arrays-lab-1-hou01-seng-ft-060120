7c9aeb9d6f95d18473db0d39a381e0637395307a
Bree Warren <ambreea.warren@gmail.com>
2020-05-16 20:50:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ end
 def matrix_lookup(matrix, row, column)
   matrix[row][column]
 end
-matrix
+matrix_lookup
 
  # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
